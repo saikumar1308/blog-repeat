5811b1936b831fdfb01b792f68f5858b776be5a2
saikumar1308 <126888243+saikumar1308@users.noreply.github.com>
2024-12-11 08:28:19 +0530

    Revert "mid commit"
    This reverts commit 44bd777647e68b5017a8682136bf8291092388d7.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,92 +1,9 @@
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono'
-import { sign, verify } from 'hono/jwt';
 
-const app = new Hono<{
-  Bindings:{
-    DATABASE_URL: string,
-    JWT_SECRET: string
-  },
-  Variables:{
-    userId:string
-  }
-}>();
+const app = new Hono()
 
-app.post('/api/v1/signup',async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl:c.env?.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-  const body = await c.req.json();
-  try {
-    const user = await prisma.user.create({
-      data:{
-        username: body.username,
-        password: body.password
-      }
-    })
-    const jwt = await sign({id:user.id},c.env.JWT_SECRET);
-    return c.json({jwt});
-  } catch (error) {
-    c.status(403)
-    return c.json({error:"errot while signing up"});
-  }
-  
-})
-
-app.post('/api/v1/signin',async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl:c.env?.DATABASE_URL
-  }).$extends(withAccelerate())
-
-  const body = await c.req.json()
-  const user = await prisma.user.findUnique({
-    where:{
-      username:body.username
-    }
-  })
-
-  if (!user) {
-    c.status(403);
-    return c.json({error:"user not found"})
-  }
-
-  const jwt = await sign({id:user.id},c.env.JWT_SECRET);
-  return c.json({jwt})
-})
-
-app.use('/api/v1/blog/*', async (c, next) => {
-  const jwt = c.req.header('Authorization');
-  if(!jwt){
-    c.status(401);
-    return c.json({error:'Unauthorised'})
-  }
-  const token = jwt.split('')[1];
-  const payload = await verify(token,c.env.JWT_SECRET);
-  if(!payload){
-    c.status(401);
-    return c.json({error:'Unauthorised'})
-  }
-  c.set('userId',payload.id);
-  await next()
-})
-
-app.post('/api/v1/blog', (c) => {
-  console.log(c.get('userId'))
-  return c.text('signin route')
-})
-
-app.put('/api/v1/user/signin', (c) => {
-  return c.text('hloo')
-})
-
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('hloo')
-})
-
-app.get('/api/v1/blog/bulk', (c) => {
-  return c.text('hloo')
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
 })
 
 export default app
